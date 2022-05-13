4d76c88c43d39caa7cb6f4513d1c88aa0591d2dd
Jack-Bai010210 <68420741+Jack-Bai010210@users.noreply.github.com>
2022-05-13 16:58:12 +0900

    gitignoreが昨日することを確認し、改めてプロジェクトの作成

--- TOFU3/Assets/Player/PlayerStatusScript.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerStatusScript
-{
-    // PlayerHPGet関数
-    private float GetPlayerHP { set; get; }
-}
