3497250921ba6ac89c2feeaae3a7fdc6459cb043
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2022-08-09 09:19:35 +0900

    自分の作業更新

--- TOFU3/Assets/Member/Ichihara/Scripts/Models/Items/YUBA_SHIELD.cs
+++ TOFU3/Assets/Member/Ichihara/Scripts/Models/Items/YUBA_SHIELD.cs
@@ -9,40 +9,34 @@ public class YUBA_SHIELD : BaseItemStatus
     [SerializeField]
     private PlayerStatus _cs_playerStatus;
 
-    private Collision _col = null;
-
     private bool _endFlag = false;
 
     private void Start()
     {
+
         //ダメージ倍率、圧力倍率を初期化
         _damageMagni = 0.0f;
         _pressForceMagni = 0.0f;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void Update()
     {
-        _col = collision;
-        this.gameObject.SetActive(false);
-        GetYUBA_SHIELD();
+        //取得したプレイヤーの周りを青くハイライト
+
+        //if (/*弾がプレイヤーに当たったら*/)
+        //{
+        //    YUBA_SHIELDEffect();
+        //    _endFlag = true;
+        //}
+
     }
 
-    private void GetYUBA_SHIELD()
+    private void YUBA_SHIELDEffect()
     {
-        do
-        {
-            //取得したプレイヤーの周りを青くハイライト
 
-            //if (/*弾がプレイヤーに当たったら*/)
-            //{
-            //    //ダメージを一回だけ０にする
-            //    SHOYOUGUN.Instance.ShotDamage *= _damageMagni;
-            //    //圧力を一回だけ０にする
-            //    SHOYOUGUN.Instance._forcePower *= _pressForceMagni;
-            //    _endFlag = true;
-            //}
-
-
-        } while (!_endFlag);
+        //ダメージを一回だけ０にする
+        SHOYOUGUN.Instance.ShotDamage *= _damageMagni;
+        //圧力を一回だけ０にする
+        SHOYOUGUN.Instance._forcePower *= _pressForceMagni;
     }
 }
