01f7892e410df871a4c9aee24866bf6feb31945d
田中 <tana.di0211@gmail.com>
2022-08-31 23:56:47 +0900

    弾の吹き飛び作成

--- TOFU3/Assets/Member/tanaka/Script/tama.cs
+++ TOFU3/Assets/Member/tanaka/Script/tama.cs
@@ -5,29 +5,27 @@ using Unity.VisualScripting;
 using UnityEditorInternal;
 using UnityEngine;
 
-public class Tama : MonoBehaviour
+public class tama : MonoBehaviour
 {
-    
-    [SerializeField]
-    private GameObject player;
     [SerializeField]
     private float knockBack;
 
     private void OnTriggerEnter(Collider other)
     {
+        //Debug.Log(other.gameObject.tag);
 
-        if (other.gameObject.name == "mato")
+        if (other.gameObject.tag == "Player")
         {
 
-            Rigidbody otherRigitbody = other.GetComponent<Rigidbody>();
+            Rigidbody otherRigitbody = other.GetComponentInParent<Rigidbody>();
 
             // 自分の位置と接触してきたオブジェクトの位置とを計算して、距離と方向を出して正規化
             Vector3 distination = (other.transform.position - transform.position).normalized;
-            Debug.Log(otherRigitbody.transform.position);
+            //Debug.Log(otherRigitbody.transform.position);
             //Debug.Log(transform.position);
             otherRigitbody.AddForce(-distination * knockBack, ForceMode.Impulse); //当たった物体
-            Debug.Log("hukitobi");
-            return;
+            //Debug.Log("hukitobi");
+            //return;
 
         }
 
